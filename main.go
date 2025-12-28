f0163cbcac7a0b9cefb1851b5118d6b7a085fa63
Nebojsa Knezevic [EXT] <nebojsa.knezevic@ch.abb.com>
2025-12-28 20:39:04 +0100

    first commit

---
+++ main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+
+	// "go/token"
+	"fmt"
+	"interpreter/repl"
+	"os"
+	"os/user"
+)
+
+func main() {
+	// var MonkeySubset string = "let "
+	// fmt.Println(MonkeySubset)
+
+	// data, err := os.ReadFile("monkey.txt")
+
+	// if err != nil {
+	// 	fmt.Println("Error occured during file reading")
+	// 	return
+	// }
+
+	// fmt.Println("Content: ")
+	// fmt.Println(string(data))
+
+	user, err := user.Current()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("Hello %s! This is the Monkey programming language!\n",
+		user.Username)
+	fmt.Printf("Feel free to type in commands\n")
+	repl.Start(os.Stdin, os.Stdout)
+
+	// input := `
+	// 10 != 9;
+	// `
+
+	// lex := lexer.New(input)
+
+	// for i := 0; i != len(input); i++ {
+
+	// 	tok := lex.NextToken() // Pozovi funkciju SAMO JEDNOM
+
+	// 	if tok.Type == token.EOF {
+	// 		break
+	// 	}
+
+	// 	fmt.Println(tok)
+	// }
+
+}
